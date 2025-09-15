4b7a9f637231563e67d29a3692865bec2fd980ce
gabriel-victurino <gabriel.gbzzx@gmail.com>
2025-09-15 11:07:17 -0300

    Exercício java.net Correção Import

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,7 +1,10 @@
 package principal;
 
-import java.net.*;
-import java.io.*;
+import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import javax.swing.JOptionPane;
 
 public class Principal {
