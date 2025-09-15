2875c2c875a9e2c0c1985b9fb589d281d5548ecb
gabriel-victurino <gabriel.gbzzx@gmail.com>
2025-09-15 09:29:58 -0300

    Exercício java.net

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,24 @@
+package principal;
+
+import java.net.*;
+import java.io.*;
+import javax.swing.JOptionPane;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        try {
+            String site = JOptionPane.showInputDialog("Digite um site para ser pesquisado.");
+            Socket sock = new Socket(site,80);
+            PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            String linha = "";
+            out.println("GET / HTTP/1.0\n");
+            while ((linha = in.readLine()) != null) {
+                System.out.println("echo: " + linha);
+            }
+        } catch (IOException e) {
+            System.err.println("Problemas de IO");
+        }
+    }
+}
